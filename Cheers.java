e6660758d811db7cbc7566add11e1554eb3119aa
NirGeller-reichman <nir.gellerchipchev@post.runi.ac.il>
2025-11-06 20:17:08 +0200

    1st

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,30 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String inputCheer = args[0];
+            String times = args[1];
+            int timesnum = Integer.parseInt(times);
+            String newcheer = inputCheer.toUpperCase();
+            String otherchercters = "AEFHILMNORSX"; 
+            int numberOfCharacters = newcheer.length();
+            int i=0;
+            while (i < numberOfCharacters) {
+                char currentLetter = newcheer.charAt(i);
+                if (otherchercters.indexOf(currentLetter) == -1) {
+                        System.out.println("Give me a " + currentLetter + ":" + currentLetter + " !"); 
+         }
+                else {
+                        System.out.println("Give me an " + currentLetter + ":" + currentLetter + " !");
+        }
+                
+                i++;
+                
+                
+            }
+                System.out.println("What does that spell?");
+                for (int j = 0; j < timesnum; j++) {
+                System.out.println(inputCheer + "!!!");
+                }
+
         }
 }
