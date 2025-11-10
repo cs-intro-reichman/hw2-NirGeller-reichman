7e26a60040d218fad8f97d1fa97fe4e0d894ed68
NirGeller-reichman <nir.gellerchipchev@post.runi.ac.il>
2025-11-10 22:33:45 +0200

    v

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,40 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+	    int Seedinput = Integer.parseInt(args[0]);
+		String mode = args[1];
+		boolean isVerbose = mode.equals("verbose");
+		boolean Isconcise = mode.equals("concise");
+		for(int cuurentseed = 1; cuurentseed <= Seedinput; cuurentseed++) {
+			int count = 0; 
+			int n = cuurentseed;
+			if (isVerbose) {
+				System.out.print(n + " ");
+			}
+			do {
+				if (n % 2 == 0) {
+					n = n / 2;
+				} 
+				else {
+					n = 3 * n + 1;
+				}
+
+				if (isVerbose) {
+					System.out.print(n + " ");
+				}
+				count = count + 1;				
+			} while (n != 1);
+			System.out.println("(" + count + ")");  
+		}
+		if (Isconcise) 
+			System.out.println("Every one of the first "+ Seedinput + "hailstone sequences reached 1.");
+		 if (isVerbose) 
+			System.out.println("Every one of the first "+ Seedinput + "hailstone sequences reached 1.");
+		 
+
+			
+			
 }
+}
+		
+	
